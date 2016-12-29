36a10aa80b63081561aa2528306d5b52199b27d3
LISZ\李双住__lisz <740763749@qq.com>
2016-12-29 13:25:44 +0800

    20161229

--- app/components/Navigation/index.jsx
+++ app/components/Navigation/index.jsx
@@ -1,17 +1,56 @@
 import React, {Component, PropTypes} from 'react';
-import { IndexLink  } from 'react-router';
+import {IndexLink} from 'react-router';
 import NavLink from './naviLink';
 /*
  * @class Navigation `导航`组件
  */
+import './style.css'
+let imageDatas = require("./imgdata.json");
+imageDatas = (function getImgUrl(imgDatasArr) {
+    for (var i = 0, j = imgDatasArr.length; i < j; i++) {
+        var oneimgData = imgDatasArr[i];
+        oneimgData.imageURL = require('./images/' + oneimgData.icon);
+        imgDatasArr[i] = oneimgData; //这是引用，可以不要，谨慎起见
+    }
+    return imgDatasArr;
+})(imageDatas);
+class Nav extends React.Component {
+    render() {
+        return (
+            <div id="nav">
+                <img src={require(this.props.icon)} id="img"/>
+                <b id="name">{this.props.name}</b>
+            </div>
+        );
+    }
+};
 export default class Navigation extends Component {
     constructor() {
         super();
     }
-    render(){
-        return(
-            <div>
+    render() {
+        return (
+          <div className="clumcontainer">
+            <div className="footer">
+                <NavLink to="/index">
+                    <Nav icon={'./images/' + imageDatas[0].icon} name={imageDatas[0].name}/>
+                </NavLink>
+                <NavLink to="/classify">
+                    <Nav icon={'./images/' + imageDatas[1].icon} name={imageDatas[1].name}/>
+                </NavLink>
+                <NavLink to="/lable">
+                    <Nav icon={'./images/' + imageDatas[2].icon} name={imageDatas[2].name}/>
+                </NavLink>
+                <NavLink to="/demo">
+                    <Nav icon={'./images/' + imageDatas[3].icon} name={imageDatas[3].name}/>
+                </NavLink>
+                <NavLink to="/footmark">
+                    <Nav icon={'./images/' + imageDatas[4].icon} name={imageDatas[4].name}/>
+                </NavLink>
             </div>
+            <div className="line" />
+          </div>
+
         )
     }
 }
--- app/main.jsx
+++ app/main.jsx
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom';
 import {Route, IndexRoute, browserHistory, Router} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-import App from './container/App.jsx';
-import AllMemosRoute from './routes/AllMemosRoute.jsx';
 import configureStore from './stores';
+import Index from './components/Index';
+import Demo from './components/Demo';
+import Footmark from './components/Footmark';
+import Classify from './components/Classify';
+import Lable from './components/Lable';
+import './style/App.css';
 const store = configureStore();
 ReactDOM.render(
     <Provider store={store}>
         <Router history={browserHistory}>
-            <Route path="/"  component={App}>
-            <IndexRoute component={AllMemosRoute}/>
+            <Route path="/" >
+              <IndexRoute   component={Index}/>
+              <Route path="/index"  component={Index}/>
+              <Route path="/demo" component={Demo}/>
+              <Route path="/lable" component={Lable}/>
+              <Route path="/classify" component={Classify}/>
+              <Route path="/footmark" component={Footmark}/>
             </Route>
         </Router>
    </Provider>,
